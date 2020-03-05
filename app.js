96600eb370e8990a31c82cef95a2987faa3adffb
Andrew Davisson <akdavisson4@gmail.com>
2020-03-05 08:42:08 -0700

    added body parser package

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const subjectRouter = require('./src/routes/subject');
 const questionRouter = require('./src/routes/question');
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const port = 3001
 
-//app.use(express.json);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Study Questions API'))
 

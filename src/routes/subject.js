73e973385e465e45dcc9f7a14cbf9c2b858fff7d
Andrew Davisson <akdavisson4@gmail.com>
2020-03-23 07:47:41 -0600

    moved functions outside of route definitions

--- src/routes/subject.js
+++ src/routes/subject.js
@@ -3,27 +3,40 @@ const db = require('../persistence/models');
 
 var router = express.Router();
 
-// index route for subjects
-router.get('/', async (req, res) => {  
-  let allSubjects = await db.Subject.findAll({
+// Functions for Routes
+const index = async () => {
+  return await db.Subject.findAll({
     attributes: ['id', 'name']
   });
+}
+
+const show = async (id) => {
+  return await db.Question.findAll({
+    where: { subjectId: id},
+    attributes: ['id', 'question', 'answer', 'subjectId']
+  });
+}
+
+const create = async(obj) => {
+  return await db.Subject.create({name: obj.name});
+}
+
+// index route for subjects
+router.get('/', async (req, res) => {  
+  let allSubjects = await index()
   res.json(allSubjects);
 });
 
 // route to display all questions belonging to a specific subject
 router.get('/:id/questions', async (req, res) => {
-  let questions = await db.Question.findAll({
-    where: { subjectId: req.params.id },
-    attributes: ['id', 'question', 'answer', 'subjectId']
-  });
-
+  let questions = await show(req.params.id);
   res.send(questions);
 })
 
 // create route for Subjects
 router.post('/', async (req, res) =>{
-  let subject = await db.Subject.create({name: req.body.name});
+  // let subject = await db.Subject.create({name: req.body.name});
+  let subject = await create(req.body);
   res.send(subject);
 })
 
